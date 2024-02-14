58864734942beec767201a1b8da236ea73c34d6d
Juan Camilo Convers Yañez <juanconvers.legal@gmail.com>
2024-02-13 23:13:09 -0500

    Commit No.5: implementando Multer y Static

--- src/app.js
+++ src/app.js
@@ -1,12 +1,27 @@
  
 import express from "express";
 import productsRouter from './routes/ProductsRouter.js';
+import { __dirname } from "./path.js";
+import upload from "./config/multer.js";
+
+console.log(__dirname)
 
 const app = express()
 const PORT = 11000
 
 app.use(express.json())
+app.use('/static', express.static(__dirname + '/public'))
+
+
 app.use('/products', productsRouter)
+app.post('/upload', upload.single('product'), (req, res) => {
+    try {
+        console.log(req.file)
+        res.status(200).send("Imagen cargada correctamente")
+    } catch (e) {
+        res.status(500).send("Error al cargar imagen")
+    }
+})
 
 app.listen(PORT,() => {
     console.log(`Server on port ${PORT}`)
